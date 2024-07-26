e9e1e3e7be60476b80892207460a5940c86e0c05
Rushi Trivedi <trivedirushi0501@gmail.com>
2024-07-26 19:55:57 +0530

    Add files via upload

---
+++ MyfirstCprogram.c
@@ -0,0 +1,15 @@
+// Hi
+//my name is rushi trivedi
+//this is my first c program
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    printf("Hello \n");
+    printf("My name is Rushi Trivedi \n");
+    printf("I am from Ahmedabad \n");
+    printf("I want to make my career in AI ML \n");
+    return 0;
+}
